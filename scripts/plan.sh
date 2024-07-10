61057c1fb1038638402c8a3114275708f1333ea7
root <root@DESKTOP-R6QB3SK.localdomain>
2024-07-10 18:48:04 +0530

    add

--- scripts/plan.sh
+++ scripts/plan.sh
@@ -1,50 +1,28 @@
 #!/bin/bash
-# Author: lars.butler@rackspace.com
-# Modified by: nick.garratt@rackspace.co.uk
 
-set -euxo pipefail
+# Set default output directory for plans (modify if needed)
+PLAN_DIR=${PLAN_DIR:-"./plans"}
 
+# Get environment and layer name from folder structure
 environment="${ENVIRONMENT_NAME}"
+layer="${PWD##*/}"
 
-# Directory of this script:
-SCRIPTS_DIR="$( cd "$(dirname "${0}")" ; pwd -P )"
+# Read AWS account ID from environment variable
+aws_account_id="${aws_account_id}"
 
-#source ${SCRIPTS_DIR}/vars.sh
-source ${SCRIPTS_DIR}/util.sh
+# Run Terraform plan with desired options
+terraform plan \
+  -no-color \
+  -input=false \
+  -var-file="envs/${environment}.tfvars" \
+  -var "aws_account_id=${aws_account_id}" \
+  -var "layer=${layer}" \
+  -out="${PLAN_DIR}/${environment}_${layer}.plan"
 
-# we may be sharing this container with other builds
-PLAN_DIR="${PLANS_DIR}/${environment}"
-mkdir -p "${PLAN_DIR}"
+# Handle potential errors
+if [ $? -ne 0 ]; then
+  echo "Terraform plan failed for ${environment}/${layer}"
+  exit 1
+fi
 
-function plan_layers {
-  local environment="${1}"
-  local layers=$(ls ${LAYERS_DIR})
-  local aws_account_id=$(get_aws_account_id)
-  local state_bucket=$(get_state_bucket $aws_account_id)
-
-  echo "DIAG: AWS_ACCOUNT_ID=${aws_account_id}"
-  echo "DIAG: ENVIRONMENT=${environment}"
-
-  for layer in $layers; do
-    local layer_dir="${LAYERS_DIR}/${layer}"
-    pushd ${layer_dir}
-
-    tf_init ${state_bucket} ${TF_STATE_REGION} ${aws_account_id} ${layer}
-
-    # create and/or switch to the appropriate terraform workspace
-    terraform workspace select ${environment} || terraform workspace new ${environment}
-
-    # NOTE: Assume that the .tfvars file exists for this environment:
-
-    terraform plan \
-      -no-color \
-      -input=false \
-      -var-file="envs/${environment}.tfvars" \
-      -var "aws_account_id=${aws_account_id}" \
-      -var "layer=${layer}" \
-      -out="${PLAN_DIR}/${environment}_${layer}.plan"
-    popd
-  done
-}
-
-plan_layers ${environment}
+echo "Terraform plan successful for ${environment}/${layer}. Plan file: ${PLAN_DIR}/${environment}_${layer}.plan"
