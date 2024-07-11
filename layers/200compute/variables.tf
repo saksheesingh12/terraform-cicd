c6ee626dba3dc99bfdafe26bdfdc25785c5bdd88
root <root@DESKTOP-R6QB3SK.localdomain>
2024-07-11 16:41:40 +0530

    add

--- layers/200compute/variables.tf
+++ layers/200compute/variables.tf
@@ -1,9 +1,9 @@
 variable "layer" {
   description = "Name of the layer to propagate to resources."
   type        = string
-  }
+}
 
 variable "aws_account_id" {
   description = "AWS Account Number"
   type        = string
-  }
+}
