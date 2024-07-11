9e2f0c8c754df5b41e449ae47c4eb8c77c843951
root <root@DESKTOP-R6QB3SK.localdomain>
2024-07-11 17:12:09 +0530

    vlidate

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -10,11 +10,19 @@ environment="${ENVIRONMENT_NAME}"
 
 #source ${SCRIPTS_DIR}/vars.sh
 layers=$(ls ${LAYERS_DIR})
+state_bucket=$(yq -r '.environments[] | select(.name == "'"$environment"'") | .bucket' environments.yml)
 
 for layer in $layers
 do
   pushd ${LAYERS_DIR}/${layer}
   echo "running on layer ${layer}."
+      terraform init \
+    -no-color \
+    -input=false \
+    -backend=true \
+    -backend-config="bucket=${state_bucket}" \
+    -backend-config="region=ap-south-1" \
+    -backend-config="key=terraform.${layer}.tfstate " \
   terraform validate
   popd
 done
